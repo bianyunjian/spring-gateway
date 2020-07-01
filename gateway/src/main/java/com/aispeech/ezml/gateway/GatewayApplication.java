3594a6439f2b7a368c6d7023c566673d2a165051
卞云健 <yunjian.bian@hankutech.com>
2020-07-01 10:15:45 +0800

    服务注册和刷新的时间间隔调整

--- gateway/src/main/java/com/aispeech/ezml/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/aispeech/ezml/gateway/GatewayApplication.java
@@ -1,21 +1,28 @@
 package com.aispeech.ezml.gateway;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.netflix.loadbalancer.ServerListUpdater;
+import com.netflix.niws.loadbalancer.EurekaNotificationServerListUpdater;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
-@EnableFeignClients()
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
-
 
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    /**
+     * ribbon默认的更新机制是通过PollingServerListUpdater维护的线程去同步，那么我们可以利用配置让ribbon使用事件监听机制更新
+     * @return
+     */
+    @Bean
+    public ServerListUpdater ribbonServerListUpdater() {
+        return new EurekaNotificationServerListUpdater();
+    }
 }
 
